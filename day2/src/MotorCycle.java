63553552bb1ad440e1eaa54e5e7c89e7469164b6
Devin Gallis <devingallis@outlook.com>
2018-04-30 15:32:22 -0400

    initial commit

---
+++ day2/src/MotorCycle.java
@@ -0,0 +1,16 @@
+
+public class Motorcycle {
+	String make;
+	String color;
+	boolean engineStatus = true;
+	
+	void startEngine() {
+		if (engineStatus == true) {
+			System.out.println("The engine is already on");
+		}
+		else {
+			engineStatus = true;
+			System.out.println("the engine is now on");
+		}
+	}
+}
